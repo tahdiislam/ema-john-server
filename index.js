8a70c050d2402689f1809cbc4963942ed3e66c7e
tahdi-islam <tshdi60@gmail.com>
2022-11-03 13:47:08 +0600

    add db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -11,6 +12,36 @@ app.use(express.json())
 app.get("/", (req, res) => {
 	res.send("Ema John simple server is running")
 })
+
+// connect to mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rgyxe1r.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+	try{
+		const productsCollection = client.db("emaJohn").collection("products")
+		
+		// get data from db
+		app.get("/products", async (req, res) => {
+			const query = {}
+			const cursor = productsCollection.find(query)
+			const storedProducts = await cursor.toArray()
+			res.send(storedProducts)
+		})
+	}
+	finally{
+
+	}
+}
+run().catch(err => console.log(err))
+
 
 app.listen(port, () => {
 	console.log(`Ema John simple server is running  or port ${port}`)
